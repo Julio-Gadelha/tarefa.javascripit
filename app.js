b8b4b8f8e93573617c0df090d3e7631cc393dcd0
julio gabriel <juliogabriel78956@gmail.com>
2025-02-19 19:30:10 -0300

    tarefa

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-let nome = "joão"
-let idade = "28"
-console.log(`ola vamos ${nome} oi la  ${idade}`)
+//let nome = "joão"
+//let idade = "28"
+//console.log(`ola vamos ${nome} oi la  ${idade}`)
+//let cidade = "são paulo"
+//cidade = "Rio de janeiro"
+//console.log(cidade);
+//se tentar retribuir uma constatante vai dar erro no terminal//
+//let cidade = " são paulo"
+//let cidades = "Rio de janeiro"
+//console.log(`uma cidade que ainda quero visitar ${cidade} sendo a maior festa de carnaval com carros alegorios vem da ${cidades}`)
+//let nome = "Maria"
+//let sobrenome = "silva"
+//console.log(`Ola,meu nome e ${nome} meu sobre nome não e tão distante nos sobrenomes de ricos ele e o ${sobrenome}`)
+let produto = "celulares"
+let preco = "1500"
+let quantidade = "2"
+console.log(`ola, você comprou ${quantidade} ${produto} por nada mais nada menos que ${preco} doll`)
