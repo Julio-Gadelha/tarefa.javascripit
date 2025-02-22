a45c09f508db243f90aad36b8f28e222b138509e
julio gabriel <juliogabriel78956@gmail.com>
2025-02-21 21:19:44 -0300

    incompleta

--- app.js
+++ app.js
@@ -11,7 +11,46 @@
 //let nome = "Maria"
 //let sobrenome = "silva"
 //console.log(`Ola,meu nome e ${nome} meu sobre nome não e tão distante nos sobrenomes de ricos ele e o ${sobrenome}`)
-let produto = "celulares"
-let preco = "1500"
-let quantidade = "2"
-console.log(`ola, você comprou ${quantidade} ${produto} por nada mais nada menos que ${preco} doll`)
+///let produto = "celulares"
+//let preco = "1500"
+//let quantidade = "2"
+//console.log(`ola, você comprou ${quantidade} ${produto} por nada mais nada menos que ${preco} doll`)
+//let statuspedido = "pendente"
+//statuspedido = "Enviado"
+//console.log(` O status do pedido agora e:${statuspedido}.`)
+//let produto = "celular"
+//let preco = "R$ 1500"
+//let quantidade = "2"
+//let total = "R$3000"
+//console.log(`
+    //Produto: ${produto}
+    //Preço: R$ ${preco}
+    //Quantidade: ${quantidade}
+    //Total: R$ ${total}
+    //`);
+    //const PI = 3.14;
+    //PI = 3.14159;
+  //  var x = 10;
+//if (true) {
+ // var x = 20;  // 'var' permite reatribuição caso queira mudar
+  //console.log(x); resultado sera 20 pois mais o var pode ter certas mudanças
+//}
+//o let usamos quase sempre pois e o mais confiavel
+ //let y = 10;
+//if (true) {
+ // let y = 20;  // 'let' tem escopo de bloco, então essa 'y' é diferente
+//}
+//console.log(y);  // Resultado: 10, porque a variável fora do bloco não foi alterada
+
+// Usando const
+//const z = 10;
+//if (true) {
+ // const z = 20;  // 'const' também tem escopo de bloco, e é imutável dentro do seu escopo
+//}
+//console.log(z);  // Resultado: 10,  pois constante fora do bloco não foi alterada
+//ESTUDAR MAIS ESSES CPNCEITOS NÃO ENTENDI MUITO BEM
+//let frase1 = "Javascripit"
+//let frase2 = "é"
+//let frase3 = "incrivel"
+//console.log(`Javascripit,é,incrivel`)
+  
